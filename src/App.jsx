a280e06fc1fdba837ca6e2d80d779fcee5bfa156
Nelman25 <jonelvillaver735@gmail.com>
2024-05-22 16:05:06 +0800

    added input and newroject component and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject";
 export default function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8 ">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
