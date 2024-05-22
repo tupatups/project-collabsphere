a6c742af51618db05fb4d14e8766e21bef63dec8
Nelman25 <jonelvillaver735@gmail.com>
2024-05-22 15:55:04 +0800

    added ProjectSidebar and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-function App() {
+import ProjectSidebar from "./components/ProjectSidebar";
+export default function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8">
+      <ProjectSidebar />
+    </main>
   );
 }
-
-export default App;
